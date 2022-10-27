5397c6bec186b082681e876167563150d6d71309
Leandro Pedroso <lpedroso@meudroz.com>
2022-10-27 13:46:33 -0300

    feat: page creation Created _locationId page

---
+++ src/pages/_locationId.vue
@@ -0,0 +1,28 @@
+<template>
+  <section class="section">
+    <span>Location {{ locationId }}</span>
+  </section>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'Location',
+
+  data(): {
+    locationId: string | null
+  } {
+    return {
+      locationId: null,
+    }
+  },
+
+  beforeMount() {
+    const { locationId } = this.$route.params
+    this.locationId = locationId
+  },
+
+  methods: {},
+})
+</script>
